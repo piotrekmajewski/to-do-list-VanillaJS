c9ca100e64583e257038b57518b317ef44137cf7
Piotr Majewski <piotrekmedia@gmail.com>
2022-03-08 00:30:42 +0100

    creating an add to the list and functional buttons

---
+++ main.js
@@ -0,0 +1,41 @@
+// selectors 
+
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+addTodo = (e) => {
+    e.preventDefault();
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo')
+
+    //li creating
+
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'test'
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    //check mark button
+
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+
+    //trash button
+
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+    trashButton.classList.add('complete-btn');
+    todoDiv.appendChild(trashButton);
+
+    //append to list
+
+    todoList.appendChild(todoDiv);
+}
+
+
+//events 
+
+todoButton.addEventListener('click', addTodo);
